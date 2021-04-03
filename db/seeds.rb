e9d05b20971b4ee0b27d5b4cec211499f6ab7100
github username <github email address>
2021-04-03 13:56:45 -0600

    meal type model, migration, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+breakfast = MealType.create(name: "Breakfast")
+morning_snack = MealType.create(name: "Morning Snack")
+lunch = MealType.create(name: "Lunch")
+afternoon_snack = MealType.create(name: "Afternoon Snack")
+dinner = MealType.create(name: "Dinner")
+midnight_snack = MealType.create(name: "Midnight Snack")
