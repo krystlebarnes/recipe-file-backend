114454fed700f7f59422ed80e75131c8906fd83e
github username <github email address>
2021-04-18 00:19:29 -0600

    added update routes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
+    # before_action :find_recipe, only: [:update]
 
     def index
         recipes = Recipe.all
@@ -14,10 +15,29 @@ class Api::V1::RecipesController < ApplicationController
         end
     end
 
+    def show
+        recipe = Recipe.find(params[:id])
+        render json: RecipeSerializer.new(recipe)
+      end
+
+    def update
+        recipe = Recipe.find(params[:id])
+        recipe.update(recipe_params)
+        if recipe.save
+            render json: RecipeSerializer.new(recipe), status: :accepted
+        else
+            render json: {errors: recipe.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
     private
 
     def recipe_params
         params.require(:recipe).permit(:name, :image_url, :description, :ingredients, :instructions, :meal_type_id)
     end
+
+    # def find_recipe
+    #     @updated_recipe = Recipe.find(params[:id])
+    # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 namespace :api do
   namespace :v1 do
-    resources :recipes, only: [:index, :create]
+    resources :recipes, only: [:index, :create, :show, :update]
     resources :meal_types, only: [:index]
   end
 end
